91601d9562c0ad14e045040ac9d245cd06a50dce
mike1tesla <buihaituyen2002hd@gmail.com>
2024-07-09 16:19:25 +0700

    Setup Firebase

--- lib/src/utils/theme/theme.dart
+++ lib/src/utils/theme/theme.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:login/src/constants/colors.dart';
 import 'package:login/src/utils/theme/widget_themes/elevated_button_theme.dart';
 import 'package:login/src/utils/theme/widget_themes/outlined_button_theme.dart';
 import 'package:login/src/utils/theme/widget_themes/text_form_field_theme.dart';
